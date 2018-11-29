a5a596de7ea893182cacf97c564271cc2d031988
hoanganhtuansoft <hoanganhtuansoft@gmail.com>
2018-11-29 14:34:37 +0700

    Update preprocess

--- TV_Utility.py
+++ TV_Utility.py
@@ -5,6 +5,13 @@ from pyvi import ViTokenizer
 
 import TV_Config
 
+def split_words(sent, language = 'vi'):
+    # change format to split words if current language is VN
+    if language == 'vi':
+        sent = ViTokenizer.tokenize(sent)
+
+    return nltk.tokenize.word_tokenize(sent)
+
 # remove html tag
 def clean_html(raw_html):
   cleanr = re.compile('<.*?>')
@@ -18,30 +25,22 @@ def read_stopwords(file_path):
     return stopwords
 
 # Based on language, get stop words and remove it.
-def remove_stopwords(sent, language = 'vi'):
+def remove_stopwords(words, language = 'vi'):
     if language == 'vi':
         stopwords = set(read_stopwords(TV_Config.STOP_WORDS))
-        sent = ViTokenizer.tokenize(sent)
-        split_words = sent.split()
     else:
         stopwords = set(nltk.corpus.stopwords.words('english'))
-        split_words = nltk.tokenize.word_tokenize(sent)
 
+   # words = nltk.tokenize.word_tokenize(sent)
+    ''' If remove special character, use the following code 
+    split_words = [x.strip(TV_Config.SPECIAL_CHARACTER).lower() for x in split_words]
+    return [word for word in split_words if word.encode('utf-8') not in stopwords]'''
 
-    try:
-        ''' If remove special character, use the following code 
-        split_words = [x.strip(TV_Config.SPECIAL_CHARACTER).lower() for x in split_words]
-        return [word for word in split_words if word.encode('utf-8') not in stopwords]'''
+    return [word for word in words if word not in stopwords]
 
-        return [word for word in split_words if word not in stopwords]
-    except TypeError:
-        return []
-
-
-def stem_words(sent):
-    split_words = nltk.tokenize.word_tokenize(sent)
+def stem_words(words):
     porter = nltk.stem.porter.PorterStemmer()
-    return [porter.stem(word) for word in split_words]
+    return [porter.stem(word) for word in words]
 
 # Input: sents: list of sentences
 # Output: list of preprocessed sentences
@@ -50,18 +49,20 @@ def pre_process_sentence(sents, language = 'vi'):
         # remove html tag
         sents[i] = clean_html(sents[i])
 
-        # remove multiple spaces
-        sents[i] = ' '.join(sents[i].split())
-
         # change to lower case
         sents[i] = sents[i].lower()
 
-        # remove stop words
-        sents[i] = ' '.join(remove_stopwords(sents[i], language))
+        # Get words
+        words = split_words(sents[i])
+
+        #remove stop words
+        words = remove_stopwords(words)
 
         # stem words
         if language != 'vi':
-            sents[i] = ' '.join(stem_words(sents[i]))
+            words = stem_words(words)
+
+        sents[i] = ' '.join(words)
 
     return sents
 
